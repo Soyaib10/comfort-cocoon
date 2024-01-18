ebb983bcb6bb3255d0601b6ee77733f17b4ebb44
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-18 15:56:33 +0600

    Using 'chi' for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,15 +5,14 @@ import (
 
 	"github.com/Soyaib10/comfort-cocoon/pkg/config"
 	"github.com/Soyaib10/comfort-cocoon/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New() // a new mux, mux is a http handler broadly called multiplexer.
-
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
-
+	mux := chi.NewRouter() // a new mux 'chi', mux is a http handler broadly called multiplexer.
+	mux.Use(middleware.Recoverer) // if some panic happens in handling requestes then this manages that.
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Soyaib10/comfort-cocoon
 
 go 1.21.5
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.0.11
