e5a81614cabd3093f56a3ec353841d57426f2200
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-19 10:57:45 +0600

    Developing our own middleware-part1

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,7 +11,10 @@ import (
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter() // a new mux 'chi', mux is a http handler broadly called multiplexer.
+
 	mux.Use(middleware.Recoverer) // if some panic happens in handling requestes then this manages that.
+	mux.Use(WriteToConsole)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Soyaib10/comfort-cocoon
 go 1.21.5
 
 require github.com/go-chi/chi/v5 v5.0.11
+
+require github.com/justinas/nosurf v1.1.1 // indirect
