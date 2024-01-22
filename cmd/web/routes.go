7c6a32eed8c9df62120cc81890fb3bfdd104c795
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-22 20:20:08 +0600

    Converting our pages to Go templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,11 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 
 	// new--
