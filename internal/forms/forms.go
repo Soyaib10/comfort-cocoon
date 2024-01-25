c71c9a5eb92e804c1974839d945c835753a2c48c
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-25 13:25:40 +0600

    Server-side form validation-part4

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/asaskevich/govalidator"
 )
 
 // Form creates a custom form struct, embeds a url.Values object
@@ -53,4 +55,11 @@ func (f *Form) MinLength(field string, length int, r *http.Request) bool {
 		return false
 	}
 	return true
+}
+
+// IsEmail checks for valid email
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -88,6 +88,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	// form.Has("first_name", r)
 	form.Required("first_name", "last_name", "email")
 	form.MinLength("first_name", 3, r)
+	form.IsEmail("email")
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
