2a3356430ba50dca3f24b384544c66771f623a4e
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-29 01:34:22 +0600

    testing

---
+++ internal/driver/driver.go
@@ -0,0 +1,61 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+)
+
+// DB holds the database connection pool
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn = 5
+const maxDbLifetime = 5 * time.Minute
+
+// ConnectSQL creates database pool for sql
+func ConnectSQL (dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	d.SetMaxOpenConns(maxOpenDbConn)
+	d.SetMaxIdleConns(maxIdleDbConn)
+	d.SetConnMaxLifetime(maxDbLifetime)
+
+	dbConn.SQL = d
+	err = testDB(d)
+	
+	// outof context insertion
+	if err != nil {
+		return nil, err
+	}
+	
+	return dbConn, nil
+}
+
+// testDB tries to ping the database
+func testDB(d *sql.DB) error {
+	err := d.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDatabase creates new databases for the appication
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
